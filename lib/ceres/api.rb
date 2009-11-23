16f733c6f92dfe0a321f6ba208d3c39b5ec9c8a1
Jens Nockert <Jens.Nockert@gmail.com>
2009-11-23 19:25:58 +0100

    Moved files

--- lib/ceres/api.rb
+++ lib/ceres/api.rb
@@ -1,5 +1,5 @@
 #
-#  ceres_api.rb
+#  api.rb
 #  This file is part of Ceres-API.
 #
 #  Ceres is free software: you can redistribute it and/or modify
@@ -20,9 +20,9 @@
 
 framework 'cocoa'
 
-require 'ceres_api/exceptions'
-require 'ceres_api/extensions'
-require 'ceres_api/urls'
+require 'ceres/ceres_api/exceptions'
+require 'ceres/ceres_api/extensions'
+require 'ceres/ceres_api/urls'
 
 module Ceres
   class API    
@@ -68,13 +68,13 @@ module Ceres
   end
 end
 
-require 'ceres_api/account'
-require 'ceres_api/character_full'
-require 'ceres_api/character_limited'
-require 'ceres_api/corporation_full'
-require 'ceres_api/corporation_limited'
-require 'ceres_api/fw'
-require 'ceres_api/map'
-require 'ceres_api/misc'
-require 'ceres_api/server'
-require 'ceres_api/starbase'
+require 'ceres/ceres_api/account'
+require 'ceres/ceres_api/character_full'
+require 'ceres/ceres_api/character_limited'
+require 'ceres/ceres_api/corporation_full'
+require 'ceres/ceres_api/corporation_limited'
+require 'ceres/ceres_api/fw'
+require 'ceres/ceres_api/map'
+require 'ceres/ceres_api/misc'
+require 'ceres/ceres_api/server'
+require 'ceres/ceres_api/starbase'
